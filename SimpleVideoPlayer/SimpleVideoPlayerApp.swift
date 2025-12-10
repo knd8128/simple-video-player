f95888f1aa61498e28ed7a25ac11c6ffd65c063f
Abraham Rubio <rza8128@hotmail.com>
2025-12-10 15:38:34 -0600

    Initial Commit

---
+++ SimpleVideoPlayer/SimpleVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleVideoPlayerApp.swift
+//  SimpleVideoPlayer
+//
+//  Created by Abraham Rubio on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
